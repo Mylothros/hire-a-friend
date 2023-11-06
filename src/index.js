b0d50105150e131030952fbdd83a070775b4ebb1
charalampos <charalampos.mylothros@navd.ai>
2023-11-06 23:52:48 +0200

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import LandingPage from './components/LandingPage/LandingPage.js';
-import SignUp from './components/SignUp/SignUp.js';
-import SignUpSuccess from './components/SignUp/SignUpSuccess.js';
-import { Routes, Route, useLocation } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import LandingPage from './components/LandingPage/LandingPage';
+import SignUp from './components/SignUp/SignUp';
+import SignUpSuccess from './components/SignUp/SignUpSuccess';
 
-import "./index.css";
+import './index.css';
 
-function App() {
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Root() {
   const location = useLocation();
 
   return (
     <TransitionGroup component={null}>
       <CSSTransition key={location.key} classNames="fade" timeout={300}>
         <Routes location={location}>
-          <Route index element ={<LandingPage />} />
+          <Route index element={<LandingPage />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/signup-success" element={<SignUpSuccess />} />  
+          <Route path="/signup-success" element={<SignUpSuccess />} />
         </Routes>
       </CSSTransition>
     </TransitionGroup>
   );
 }
 
-// Placed the context provider here so that <App/> can call useLocation()
-const Root = () => <BrowserRouter><App /></BrowserRouter>; // prettier-ignore
-
-render(<Root />, document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  </React.StrictMode>
+);
