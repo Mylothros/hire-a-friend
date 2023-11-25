ccb5d3b0431987e6236c560a749b59d1a93dc5dc
charalampos <charalampos.mylothros@navd.ai>
2023-11-26 00:21:49 +0200

    migrate to nextjs

---
+++ pages/index.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import Image from 'next/image'
+
+import Logo from 'components/LogoSvg.js';
+import Circle from 'components/CircleSvg.js';
+import LinkedIn from 'components/LinkedInSvg.js';
+import Instagram from 'components/InstagramSvg.js';
+import Button from 'components/Button.js';
+import Card from 'components/Card.js';
+
+import styles from 'styles/pages/landingPage.module.scss';
+import grid_1_image from "public/assets/images/grid_1.png";
+import grid_2_image from "public/assets/images/grid_2.png";
+
+import Layout from 'components/Layout'; // Path to your Layout component
+
+const LandingPage = () => {
+
+    const card1Slot = {
+        word1: "Step 1",
+        word2: "Sign-up",
+        word3: "*as a client or professional"
+    }
+
+    const card2Slot = {
+        word1: "Step 2",
+        word2: "Find your",
+        word3: "perfect",
+        word4: "friend",
+        word5: "*buy or sell services"
+    }
+
+    const card3Slot = {
+        word1: "Step 3",
+        word2: "Have a",
+        word3: " Blast!",
+        word4: "*start communicating",
+        word5: "and enjoy your new",
+        word6: "connection"
+    }
+
+    return (
+        <Layout >
+        <div className={styles['container']}>
+            <div className={styles['area-1']}>
+                <div className={styles['grid-item']}>
+                    <Logo />
+                    <p className={styles['target-paragraph-1']}>
+                        <span>Where friendship </span>
+                        <span>MEETS </span>
+                        <span><br/>opportunity </span>
+                    </p>
+                    <p className={styles['target-paragraph-2']}>
+                        <span>Join as </span>
+                        <span>a professional or as client.</span>
+                        <span>Earn money, <br/>find new connections and experience.</span>
+                    </p>
+                    <Button slot1="Sign up for free"></Button>
+                </div>
+                <div className={styles['image']}>
+                    <Image src={grid_1_image} className={styles['img']} alt="Landing Image" />
+                </div>
+            </div>
+
+            <div className={styles['area-2']}>
+                <div>
+                    <p className={styles['target-paragraph-1']}>
+                        <span>What is <br/></span>
+                        <span>Hire a friend?</span>
+                    </p>
+                    <div className={styles['target-paragraph-2']}>
+                        <div className={styles['target-paragraph-2-list']}>
+                            <Circle /><span>Platform  for buying or selling services. From <br/><div>private piano lessons to hang-out buddy.</div></span>
+                        </div>
+                  
+                        <div className={styles['target-paragraph-2-list']}>
+                        <Circle /><span>Possibility to use your skills and make <br/> <div>money doing what you love.</div></span>
+                        </div>
+              
+                        <div className={styles['target-paragraph-2-list']}>
+                        <Circle /><span>Possibility to hire a professional and get <br/> <div>services of different categories.</div></span>
+                        </div>
+                
+                        <div className={styles['target-paragraph-2-list']}>
+                        <Circle /><span>Platform  for  learning and experiencing new <br/> <div>things with a trustworthy professional.</div></span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['image']}>
+                    <Image src={grid_2_image} className={styles['img']} alt="Landing Image" />
+                </div>
+            </div>
+            <div className={styles['area-3']}>
+                
+                    <p className={styles['target-paragraph']}>
+                        <span>How do you get </span>
+                        <span>started with us.<br/></span>
+                        <span>You can create account for yourself just in 3 simple steps</span>
+                    </p>
+                        <div className={styles['image-container']}>
+                            <Card slot1={card1Slot}/>
+                            <Card slot2={card2Slot}/>
+                            <Card slot3={card3Slot}/>
+                        </div>
+                    <Button slot2="Sign up" /> </div>
+                <div className={styles['area-4']}>
+                    <div className={styles['card']}>
+                        <p>
+                            Connect with us 
+                            <LinkedIn />
+                            <Instagram />
+                            <br/>
+                            <a href="mailto:hireafriend.team@gmail.com">hireafriend.team@gmail.com</a>
+                        </p>  
+                    </div>
+                </div>
+           
+        </div></Layout>
+    );
+}
+
+export default LandingPage;
---
+++ pages/signup.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import axios from 'axios';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Logo from 'components/LogoSvg.js';
+
+import styles from '../styles/pages/signUp.module.scss';
+import sign_up from "../public/assets/images/sign_up.png";
+
+const Article = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  const isValidEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const handleChange = event => {
+    setEmail(event.target.value);
+  };
+
+  const handleHomePage = () => {
+    router.push('/');
+  };
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(null);
+    if (isValidEmail(email)) {
+      try {
+        console.log(process.env.NEXT_PUBLIC_APP_URL)
+        const response = await axios.post(process.env.NEXT_PUBLIC_APP_URL, { email });
+        if (response.status === 200) {
+          // Use Next.js router for navigation
+          // Make sure to import useRouter from 'next/router'
+          useRouter().push('/signup-success');
+        } else {
+          setError('Our API has a problem, please try again later!!!');
+        }
+      } catch (error) {
+        setError('Our API has a problem, please try again later!!!');
+      }
+    } else {
+      setError('Email is invalid');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <div className={styles['sign-up-area-1']}> {/* Use CSS Module classes */}
+      <div className={styles['sign-up-grid-item']}>
+      <Logo />
+        <div className={styles['sign-up-highlighted-word-grid-1']}>
+          Be one of the first users !
+        </div>
+        <p>Pre-registration is open</p>
+        <p style={{ fontSize: '15px' }}>Email*</p>
+        <input
+          placeholder="youremail@com"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+        {error && <h3 style={{ color: 'red' }}>{error}</h3>}
+        <div className={styles['sign-up-grid-1-button']}>
+          <button className={styles['sign-up-arrow-button']}>Sign up</button>
+        </div>
+      </div>
+      <div className={styles['sign-up-grid-item-image']}>
+        <Image src={sign_up} alt="Landing Image" /> {/* Using Next.js Image component */}
+      </div>
+    </div>
+  </form>
+  );
+};
+
+export default Article;
