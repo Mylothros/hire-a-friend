59dd33d1d7be27358369a6033113ee3c624add3c
charalampos <charalampos.mylothros@navd.ai>
2024-01-26 10:53:49 +0200

    try a different support for language

--- components/Layout.js
+++ components/Layout.js
@@ -11,12 +11,14 @@ const Layout = ({ children, title, description, ogTitle, ogDescription }) => {
   return (
     <div className={`fade-in ${fadeIn ? 'fade-in-entered' : ''}`}>
       <Head>
+      <html lang='en' >
       <title>{title || 'Hire A Friend'}</title>
         <meta name="description" content={description || "A page to make new friends"} key="desc" />
         <meta property="og:title" content={ogTitle || "Hire A Friend"} />
         <meta property="og:description" content={ogDescription || "An easy way to connect"} />
         <meta property="og:image" content={"http://test-bucket-hire.s3-website-us-east-1.amazonaws.com/" + "assets/images/grid_1.png"}/>
         {/* <meta property="og:image" content="http://test-bucket-hire.s3-website-us-east-1.amazonaws.com/assets/images/grid_1.png" /> */}
+        </html>
       </Head>
       {children}
     </div>
